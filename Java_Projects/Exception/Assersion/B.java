9c1412b77252fd2e7743e79f313ee43e52a9b073
hemant61201 <hemant61201@gmail.com>
2023-02-10 21:16:00 +0530

    10Feb_Programs

---
+++ Java_Projects/Exception/Assersion/B.java
@@ -0,0 +1,7 @@
+public class B {
+
+    void test(int i) {
+        assert i > 0 : "invalid i in B.test";
+    }
+
+}
---
+++ Java_Projects/Exception/Assersion/C.java
@@ -0,0 +1,6 @@
+public class C {
+
+    public void test(int i) {
+        assert i > 0 : "invalid i in C.test";
+    }
+}
