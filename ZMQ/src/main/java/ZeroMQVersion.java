6a902601118af050c06d777829745e9d5a571a20
hemant61201 <hemant61201@gmail.com>
2023-04-10 22:07:05 +0530

    April 10 ZMQ

---
+++ ZMQ/src/main/java/ZeroMQVersion.java
@@ -0,0 +1,17 @@
+import org.zeromq.ZMQ;
+
+//  Report 0MQ version
+public class ZeroMQVersion
+{
+    public static void main(String[] args)
+    {
+        String version = ZMQ.getVersionString();
+        int fullVersion = ZMQ.getFullVersion();
+
+        System.out.println(
+                String.format(
+                        "Version string: %s, Version int: %d", version, fullVersion
+                )
+        );
+    }
+}
