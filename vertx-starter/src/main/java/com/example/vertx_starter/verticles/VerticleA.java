96a530103ce4fa2d079402d883122c5fe74cce43
hemant61201 <hemant61201@gmail.com>
2023-05-04 21:59:22 +0530

    4 May JQuery

---
+++ vertx-starter/src/main/java/com/example/vertx_starter/verticles/VerticleA.java
@@ -0,0 +1,27 @@
+package com.example.vertx_starter.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class VerticleA extends AbstractVerticle
+{
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception
+    {
+        System.out.println("Start " + getClass().getName());
+
+        vertx.deployVerticle(new VerticleAA(), whenDeployed -> {
+
+          System.out.println("Deployed" + VerticleAA.class.getName());
+
+          vertx.undeploy(whenDeployed.result());
+        });
+
+        vertx.deployVerticle(new VerticleAB(), whenDeployed -> {
+
+          System.out.println("Deployed" + VerticleAA.class.getName());
+        });
+
+        startPromise.complete();
+    }
+}
---
+++ vertx-starter/src/main/java/com/example/vertx_starter/verticles/VerticleB.java
@@ -0,0 +1,18 @@
+package com.example.vertx_starter.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class VerticleB extends AbstractVerticle
+{
+    public class VerticleA extends AbstractVerticle
+    {
+        @Override
+        public void start(Promise<Void> startPromise) throws Exception
+        {
+          System.out.println("Start " + getClass().getName());
+
+          startPromise.complete();
+        }
+    }
+}
